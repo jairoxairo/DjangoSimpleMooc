4804cd6eb546876fb42b9a9157508be4d16dcaec
xairo <xairo_jairo@hotmail.com>
2018-09-15 02:37:22 -0300

    15/09

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -14,7 +14,9 @@ class Course(models.Model):
     
     slug = models.SlugField('Atalho')
     
-    description = models.TextField('Descrição', blank=True)
+    about = models.TextField('Sobre o Curso', blank=True)
+
+    description = models.TextField('Descrição Simples', blank=True)
     
     start_date = models.DateField(
         'Data de Início', null=True, blank=True
@@ -30,7 +32,7 @@ class Course(models.Model):
     #para acertar o nome no admin exibicao
     def __str__(self):
         return self.name
-    #muda o nome de courses para curso
+    #muda o nome de courses para curso interfere no django admin
     class Meta:
         verbose_name = 'Curso'
         verbose_name_plural = 'Cursos'
--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from simplemooc.core import views #importa de simplemooc.core.views as views
 from simplemooc.core import urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include(urls, namespace='core')),
     path('cursos/', include('simplemooc.courses.urls', namespace='courses')),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
